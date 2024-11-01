8da6295799274050a39ce083e70470f88542573a
soto <syuweb522@gmail.com>
2024-11-02 02:18:54 +0900

    adminにアクセスするurlの変更

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('management-admin/', admin.site.urls),
     path('blog/', include("blog.urls")),
     path('accounts/', include("accounts.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
